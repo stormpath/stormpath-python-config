674f3359e2bd041295e0dd07482c2fbde9eb2837
Ana Vojnovic <ana.vojnovic@dobarkod.hr>
2015-12-03 17:46:26 +0100

    Added EnrichIntegrationConfigStrategy.

--- tests/tests.py
+++ tests/tests.py
@@ -489,3 +489,90 @@ class EnrichClientFromRemoteConfigStrategyTest(TestCase):
         ecfrcs.process(config)
 
         self.assertEqual(config['application'], self.application)
+
+
+class EnrichIntegrationConfigStrategyTest(TestCase):
+    def test_enrich_client_from_integration_config_empty_user_config(self):
+        config = {'website': True, 'api': True, 'client': {'k': 'v'}}
+
+        eics = EnrichIntegrationConfigStrategy({})
+        eics.process(config)
+        self.assertEqual(
+            config,
+            {
+                'website': True,
+                'api': True,
+                'client': {'k': 'v'},
+                'web': {
+                    'register': {'enabled': True},
+                    'login': {'enabled': True},
+                    'logout': {'enabled': True},
+                    'me': {'enabled': True},
+                    'oauth2': {'enabled': True},
+                },
+            }
+        )
+
+    def test_enrich_client_from_integration_config_with_user_config_web(self):
+        config = {'website': True, 'client': {'k': 'v'}}
+        user_config = {'web': {}}
+
+        eics = EnrichIntegrationConfigStrategy(user_config)
+        eics.process(config)
+        self.assertEqual(
+            config,
+            {
+                'website': True,
+                'client': {'k': 'v'},
+                'web': {
+                    'register': {'enabled': True},
+                    'login': {'enabled': True},
+                    'logout': {'enabled': True},
+                    'me': {'enabled': True},
+                },
+            }
+        )
+
+    def test_enrich_client_from_integration_config_user_config_feature(self):
+        config = {'website': True, 'client': {'k': 'v'}}
+        user_config = {'web': {'register': {'k': 'v'}}}
+
+        eics = EnrichIntegrationConfigStrategy(user_config)
+        eics.process(config)
+        self.assertEqual(
+            config,
+            {
+                'website': True,
+                'client': {'k': 'v'},
+                'web': {
+                    'register': {'enabled': True},
+                    'login': {'enabled': True},
+                    'logout': {'enabled': True},
+                    'me': {'enabled': True},
+                },
+            }
+        )
+
+    def test_enrich_client_from_integration_config_user_config_data(self):
+        config = {'website': True, 'client': {'k': 'v'}}
+        user_config = {
+            'web': {
+                'register': {'enabled': True, 'uri': '/users-register-uri'}
+            }
+        }
+
+        eics = EnrichIntegrationConfigStrategy(user_config)
+        eics.process(config)
+        self.assertEqual(
+            config,
+            {
+                'website': True,
+                'client': {'k': 'v'},
+                'web': {
+                    'register': {'enabled': True},
+                    'login': {'enabled': True},
+                    'logout': {'enabled': True},
+                    'me': {'enabled': True},
+                },
+            }
+        )
