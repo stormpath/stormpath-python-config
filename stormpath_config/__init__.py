b2d1d829f588c5486cc1169bfbf137d34f991a90
Randall Degges <r@rdegges.com>
2016-03-17 11:30:43 -0700

    Cleaning up.

--- stormpath_config/__init__.py
+++ stormpath_config/__init__.py
@@ -1,5 +1,8 @@
+"""Python package definition."""
+
+
 __author__ = 'Stormpath, Inc.'
-__copyright__ = 'Copyright 2012-2015 Stormpath, Inc.'
+__copyright__ = 'Copyright 2012-2016 Stormpath, Inc.'
 
 __version_info__ = ('0', '0', '1')
 __version__ = '.'.join(__version_info__)
